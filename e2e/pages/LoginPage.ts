d60c9f1d00b5d36bc29fd485190101435374688b
Alexi118 <cuongnd118@gmail.com>
2023-12-21 13:16:27 +0700

    web prevent automation?

--- e2e/pages/LoginPage.ts
+++ e2e/pages/LoginPage.ts
@@ -1,18 +1,36 @@
 import {Locator, Page} from '@playwright/test';
 import { baseURL } from '../data/constant';
+import { expect } from '@playwright/test';
 
 export class LoginPage{
     readonly page: Page;
-
+    readonly account_icon: Locator;
+    readonly email_txt: Locator;
+    readonly pass_txt: Locator;
+    readonly signin_btn: Locator;
+    readonly logout_btn: Locator;
+    readonly spinning_icon: Locator;
 
     constructor(page: Page){
         this.page = page;
+        this.account_icon = page.locator("#myAccount svg");
+        this.email_txt = page.locator("#email");
+        this.pass_txt = page.locator("#password");
+        this.signin_btn = page.locator(".MyAccountOverlay-SignInButton button");
+        this.logout_btn = page.getByText('Logout');
+        this.spinning_icon = page.locator('.Loader-Main');
     }
 
     async goToLoginPage(){
         this.page.goto('/customer/account/login');
     }
 
-    async action_Loginsuccessfully_ThenRedirectToHomePage(user: string, pass: string){
+    async action_Login_ThenRedirectToHomePage(email: string, pass: string){
+        await this.goToLoginPage();
+        await this.email_txt.fill(email);
+        await this.pass_txt.fill(pass);
+        await this.signin_btn.click();
+        await expect(this.spinning_icon).toBeHidden();
+        await expect(this.logout_btn).toBeVisible();
     }    
 }
--- e2e/testcases/LoginOnLoginPage.spec.ts
+++ e2e/testcases/LoginOnLoginPage.spec.ts
@@ -2,12 +2,11 @@ import { expect, test} from '../common/fixtures';
 import data from "../data/loginData.json"
 
 
-test.beforeEach('Go to homepage', async ({homePage,page})=>{
-    await homePage.action_Login(data.correctUser.name,data.correctUser.pass);
+test.beforeEach('Go to loginPage', async ({loginPage,page})=>{
+    await loginPage.action_Login_ThenRedirectToHomePage(data.correctUser.name,data.correctUser.pass);
 })
-test("Login successfully by SignIn on HomePage link", async ({loginPage,page})=>{
-    await loginPage.goToLoginPage();
-    await expect(page.getByText('Logout')).toBeVisible();
+test("Login successfully by SignIn on Login link", async ({loginPage,page})=>{
+    await expect(loginPage.logout_btn).toBeVisible();
 })
 // test("Login UnSuccessfully caused by empty email field", async ({page,baseURL})=>{
 // })
